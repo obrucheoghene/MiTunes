62460f82b5abbadefd54393f8be3da16fb14e4b8
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-06 17:34:34 +0100

    feat: Add toast on logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ import useAuthModal from "@/hooks/useAuthModal";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUser";
 import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -26,7 +27,9 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         router.refresh();
 
         if (error) {
-            console.log(error);
+            toast.error(error.message);
+        }else{
+            toast.success('Logged out!');
         }
     };
 
