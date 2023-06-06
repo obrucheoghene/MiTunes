7b202f292e372d851794c0d6f77cab16340b4efe
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-06 09:47:12 +0100

    feat: Wrap sidebar with supabaseprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import { Figtree } from 'next/font/google'
+import SupabaseProvider from '@/providers/SubapaseProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -17,9 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <SupabaseProvider>
         <Sidebar>
           {children}
         </Sidebar>
+        </SupabaseProvider>
+
       </body>
     </html>
   )
