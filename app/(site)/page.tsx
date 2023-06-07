6bd84721a8fe25c076aae677042c9ced9c466222
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 22:22:43 +0100

    feat: Add page error handler component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,6 +6,7 @@ import PageContent from "@/app/(site)/components/PageContent";
 export const revalidate = 0 // disable cache
 export default async function Home() {
   const songs = await getSongs()
+
   return (
     <div className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
