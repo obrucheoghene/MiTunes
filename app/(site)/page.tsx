29fefd685daf41f91bfa9f97c8a2496b8a020842
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 08:08:27 +0100

    fix: Disable cache in page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
+export const revalidate = 0 // disable cache
 export default function Home() {
   return (
     <div className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
