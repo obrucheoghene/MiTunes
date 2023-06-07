04fa87646feb531fca65850ae91e57b68c5b0715
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 16:34:53 +0100

    feat: Add searchInput

---
+++ components/SearchInput.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import qs from "query-string"
+import Input from "./Input";
+const SearchInput = () => {
+    const router = useRouter();
+
+    const [value, setValue] = useState<string>("");
+    const debouncedValue = useDebounce<string>(value, 500);
+
+    useEffect(() => {
+        const query = {
+            title: debouncedValue,
+        }
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query: query
+        })
+
+        router.push(url);
+    }, [debouncedValue, router])
+    return (
+        <Input placeholder="What do you want to listen to?" 
+        value={value}
+        onChange={(e)=> setValue(e.target.value)}/>
+    )
+}
+
+export default SearchInput;
