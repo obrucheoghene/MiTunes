63ade061874ebb52cc0f1f99f53d3fe97dddfb4e
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 16:34:12 +0100

    feat: Add MediaItem component

--- components/Library.tsx
+++ components/Library.tsx
@@ -4,7 +4,13 @@ import {AiOutlinePlus} from 'react-icons/ai'
 import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
-const Library = () => {
+import { Song } from '@/types';
+import MediaItem from './MediaItem';
+
+interface librayProps {
+    songs: Song[]
+}
+const Library: React.FC<librayProps> = ({songs}) => {
     const authModal = useAuthModal();
     const {user} = useUser()
     const uploadModal = useUploadModal();
@@ -30,7 +36,14 @@ const Library = () => {
             
         </div>
         <div className=' flex flex-col gap-y-2 mt-4 px-3'>
-    List of Songs
+                {
+                    songs.map((item) => (
+                       <MediaItem onClick={() => {}}
+                       key={item.id}
+                       data={item}
+                       />
+                    ))
+                }
         </div>
     </div>
   )
