dd70d9d2bd045e6a55f2f632633bfc07bf868864
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 19:46:26 +0100

    feat: Add page content

---
+++ app/(site)/components/PageContent.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Song } from "@/types"
+import SongItem from "../../../components/SongItem"
+import useOnPlay from "@/hooks/useOnPlay"
+
+interface pageContentProps {
+    songs: Song[]
+}
+const PageContent: React.FC<pageContentProps> = ({songs}) => {
+    const onPlay  =  useOnPlay(songs);
+    if (songs.length === 0) {
+        return <div className="mt-4 text-neutral-400">
+            No songs available
+        </div>
+    }
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3
+     md:grid-cols-3 lg:grid-cols4 xl:grid-cols-5
+     2xl:grid-cols-8 gap-4 mt-4">
+        {songs.map((item) => (
+            <SongItem key={item.id} data={item} onClick={(id: string) => onPlay(id)}/>
+        ))}
+     </div>
+  )
+}
+
+export default PageContent
