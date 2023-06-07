16c5a35898ff2b0088cbdf6c89d4a36d1652d0cb
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 16:32:33 +0100

    feat: Use getSongByUserId

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import SupabaseProvider from '@/providers/SubapaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
+import getSongsByUserId from '@/actions/getSongsByUserId'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -13,11 +14,15 @@ export const metadata = {
   description: 'Listen to music',
 }
 
-export default function RootLayout({
+export const revalidate = 0 //prevent caching
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const userSongs = await getSongsByUserId();
   return (
     <html lang="en">
       <body className={font.className}>
@@ -25,7 +30,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider/>
-          <Sidebar>
+          <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
           </UserProvider>
