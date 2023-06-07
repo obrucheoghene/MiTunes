91cec385cb868d7497df2469360f520799382d88
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 09:51:18 +0100

    feat: Use get song action

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,11 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "@/components/PageContent";
 
 export const revalidate = 0 // disable cache
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs()
   return (
     <div className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -22,7 +25,7 @@ export default function Home() {
         <div className=" flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
         </div>
-      <div>List of songs</div>
+      <PageContent songs={songs}/>
       </div>
     </div>
   )
