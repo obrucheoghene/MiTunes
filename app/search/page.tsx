1f06cbf6f45a11e818ed93315eaa0e83c0886c7e
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 17:19:23 +0100

    fix: Disable caching

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,6 +8,7 @@ interface SearchProps {
         title: string;
     }
 }
+export const revalidate = 0;
 
 const Search = async ({searchParams} : SearchProps) => {
     const songs = await getSongsByTitle(searchParams.title);
