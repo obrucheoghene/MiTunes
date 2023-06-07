439815d2c46c3309e0a66e2e1f50b65ebbf24ea7
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 17:52:18 +0100

    feat: Add player component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
 import getSongsByUserId from '@/actions/getSongsByUserId'
+import Player from '@/components/Player'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -33,6 +34,8 @@ export default async function RootLayout({
           <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
+
+            <Player/>
           </UserProvider>
 
         </SupabaseProvider>
---
+++ components/Player.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import useGetSongById from "@/hooks/useGetSongById"
+import useLoadSongUrl from "@/hooks/useLoadSongUrl"
+import usePlayer from "@/hooks/usePlayer"
+
+const Player = () => {
+
+    const player = usePlayer()
+    const {song} = useGetSongById(player.activeId)
+
+    const songUrl = useLoadSongUrl(song!)
+
+    if(!song || !songUrl || !player.activeId){
+        return null;
+    }
+  return (
+    <div className=" fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">P layer</div>
+  )
+}
+
+export default Player
