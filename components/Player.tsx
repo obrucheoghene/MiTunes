b0bd37621d40f0186836928c409565cedaad2972
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 19:47:43 +0100

    feat: Use player content in player

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,6 +3,7 @@
 import useGetSongById from "@/hooks/useGetSongById"
 import useLoadSongUrl from "@/hooks/useLoadSongUrl"
 import usePlayer from "@/hooks/usePlayer"
+import PlayerContent from "./PlayerContent"
 
 const Player = () => {
 
@@ -15,7 +16,14 @@ const Player = () => {
         return null;
     }
   return (
-    <div className=" fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">P layer</div>
+    <div className=" fixed bottom-0 bg-black w-full 
+    py-2 h-[80px] px-4">
+      <PlayerContent     
+      key={songUrl}
+      song={song}
+      songUrl={songUrl}
+      />
+    </div>
   )
 }
 
