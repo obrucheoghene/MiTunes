fd10243755d1114efb37c817ddb2a4f1b8f8eddf
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 20:18:31 +0100

    feat: Implement playlist for searched songs

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -2,12 +2,14 @@
 
 import LikeButton from "@/components/LikeButton"
 import MediaItem from "@/components/MediaItem"
+import useOnPlay from "@/hooks/useOnPlay"
 import { Song } from "@/types"
 
 interface SearchContentProps {
     songs: Song[]
 }
 const SearchContent: React.FC<SearchContentProps> = ({songs}) => {
+    const onPlay = useOnPlay(songs)
     if (songs.length === 0) {
         return (
             <div className="flex flex-col gap-y-2 w-full
@@ -16,13 +18,14 @@ const SearchContent: React.FC<SearchContentProps> = ({songs}) => {
             </div>
         )
     }
+    
     return (
         <div className="flex flex-col gap-y-2 w-full px-6">
             {songs.map((song) => (
                 <div key={song.id} className="flex items-center
                 gap-x-4 w-full">
                     <div className="flex-1">
-                        <MediaItem onClick={() => {}} data={song}/>
+                        <MediaItem onClick={(id: string) => onPlay(id)} data={song}/>
                     </div>
                    <LikeButton songId={song.id}/>
                 </div>
