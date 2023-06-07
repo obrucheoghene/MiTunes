77d9566f86611bafd3cce92d027c538684b4db6c
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-07 20:18:16 +0100

    feat: Implement playlist for liked songs

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -7,6 +7,7 @@ import { useEffect } from "react"
 import { useUser } from "@/hooks/useUser"
 import MediaItem from "@/components/MediaItem"
 import LikeButton from "@/components/LikeButton"
+import useOnPlay from "@/hooks/useOnPlay"
 
 interface LikedContentProps {
     songs: Song[]
@@ -14,6 +15,7 @@ interface LikedContentProps {
 const LikedContent: React.FC<LikedContentProps> = ({songs}) => {
     const  router = useRouter()
     const {isLoading, user} = useUser();
+    const onPlay = useOnPlay(songs)
 
     useEffect(() => {
         
@@ -34,7 +36,7 @@ const LikedContent: React.FC<LikedContentProps> = ({songs}) => {
             <div key={song.id} className="flex items-center 
             gap-x-4 w-full">
                 <div className="flex-1"> 
-                    <MediaItem onClick={() => {}} data={song} />
+                    <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
                 </div>
                 <LikeButton songId={song.id} />
             </div>
