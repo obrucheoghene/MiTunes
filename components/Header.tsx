02cd55f0d746072b7dceb5284496f60e97af22ea
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-11 05:43:16 +0100

    fix: Add router to home and searh icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,10 +54,10 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
                     </button>
                 </div>
                 <div className=" flex md:hidden gap-x-2 items-center">
-                    <button className=" rounded-full p-2 flex items-center bg-white justify-center hover:opacity-75 transition">
+                    <button onClick={() => router.push('/')} className=" rounded-full p-2 flex items-center bg-white justify-center hover:opacity-75 transition">
                         <HiHome className="text-black" size={20} />
                     </button>
-                    <button className=" rounded-full p-2 flex items-center bg-white justify-center hover:opacity-75 transition">
+                    <button onClick={() => router.push('/search')} className=" rounded-full p-2 flex items-center bg-white justify-center hover:opacity-75 transition">
                         <BiSearch className="text-black" size={20} />
                     </button>
                 </div>
