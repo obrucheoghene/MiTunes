03ecd306f193c4731b8c76a9bf5d3bd70cf5f8d9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-12 17:53:52 +0100

    feat: Add verify account page

---
+++ app/verify/page.tsx
@@ -0,0 +1,12 @@
+
+import Header from '@/components/Header';
+
+export default async function Verify() {
+    return (
+        <div className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+            <Header>
+            </Header>
+        </div>
+    )
+}
+
