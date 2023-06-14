1417a2439c016ea1ca91cac80e78816ce6aa9a54
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-14 16:32:07 +0100

    refactor: Update

--- actions/getSongs.ts
+++ actions/getSongs.ts
@@ -3,18 +3,18 @@ import { appwriteConfig } from "@/libs/configs";
 import { Song } from "@/types";
 import { Query } from "node-appwrite";
 const getSongs = async (): Promise<Song[]> => {
-    const {databaseId, songsCollectionId} = appwriteConfig
+    const { databaseId, songsCollectionId } = appwriteConfig
 
     try {
-        const response = await appwriteServerClientDatabases.listDocuments(databaseId, songsCollectionId, [Query.orderDesc('$createdAt')] )
+        const response = await appwriteServerClientDatabases.listDocuments(databaseId, songsCollectionId, [Query.orderDesc('$createdAt')])
         if (response.documents.length) {
-            const data = response.documents.map((item) => ({...item, id: item.$id})) 
+            const data = response.documents.map((item) => ({ ...item, id: item.$id }))
             return data as any
         }
     } catch (error) {
         console.log(error)
     }
-     return [];
+    return [];
 }
 
 export default getSongs;
